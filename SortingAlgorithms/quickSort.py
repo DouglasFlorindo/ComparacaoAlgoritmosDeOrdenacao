e6bef5d33807a534ed5de8feacf64de3f7f9a9ca
Giovanna Moya <83098535+missGMoya@users.noreply.github.com>
2024-10-08 22:13:27 -0300

    quickSort.py
    desculpa douglas KLKKKKk

--- SortingAlgorithms/quickSort.py
+++ SortingAlgorithms/quickSort.py
@@ -1,9 +1,44 @@
+
+def __call__(self, data: array):
+    self.quick_sort(data)
+
+    def partition(array, low, high):
+        pivot = array[high]
+        i = low - 1
+
+        for j in range(low, high):
+            if array[j] <= pivot:
+                i += 1
+                array[i], array[j] = array[j], array[i]
+
+        array[i+1], array[high] = array[high], array[i+1]
+        return i+1
+
+    def quicksort(array, low=0, high=None):
+        if high is None:
+            high = len(array) - 1
+
+        if low < high:
+            pivot_index = partition(array, low, high)
+            quicksort(array, low, pivot_index-1)
+            quicksort(array, pivot_index+1, high)
+
+
+
+
+
+
+
+
+
+"""
 class QuickSort:
+    lista = [5, 4, 3, 2, 1]
+    def __call__(self, lista):
+        self.quick_sort(lista)
+        return lista
+    def quick_sort(self, lista):
 
-    def __call__(self, data: list) -> list:
-        return self.quick_sort(data)
-
-    def quick_sort(self, lista: list) -> list:
         print(lista)
         tamanho_da_lista = len(lista)
         if tamanho_da_lista > 0:
@@ -30,4 +65,50 @@ class QuickSort:
 
                 lista[anterior] = pivo
             print(lista)
+
             return lista
+"""
+
+##lista = [5,4,7,2,1]
+
+"""lista = [8, 2, 3, 7,9]
+def quicksort(lista):
+    listaOrdenada=[]
+    menores = []
+    maiores=[]
+    igual=[]
+
+    pivo = len(lista)//2
+    pivo = lista[pivo+1]
+    print(pivo)
+
+    for i in lista:
+        if i < pivo:
+            menores.append(i)
+        if i == pivo:
+            igual.append(i)
+        if i > pivo:
+
+            maiores.append(i)
+
+
+
+    for me in menores:
+        listaOrdenada.append(me)
+    for ig in igual:
+
+        listaOrdenada.append(ig)
+    for ma in maiores:
+
+        listaOrdenada.append(ma)
+
+    print(listaOrdenada)
+
+
+quicksort(lista)
+print("oioioio")
+"""
+
+
+
+#Python
