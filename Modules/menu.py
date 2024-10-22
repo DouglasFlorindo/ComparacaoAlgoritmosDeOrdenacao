0f7f54d0dd1e97f75f484942a76b49173dccfd68
Douglas Florindo <douglasflorindodeassis@gmail.com>
2024-10-22 19:09:11 -0300

    fghdfgjdy

--- Modules/menu.py
+++ Modules/menu.py
@@ -3,4 +3,62 @@
 class menu:
 
     def __init__(self):
-        pass
+        self.data_list: list = self.get_data('data.txt')
+        
+
+    def get_data(file_name: str) -> list:
+        try:
+            raw: str = ''
+            with open('data.txt') as f:
+                raw = f.read()
+                f.close()
+            raw_list: list = raw.split(',')
+            int_list: list = []
+            for item in raw_list:
+                int_list.append(int(item))
+            return int_list
+        except Exception as e:
+            print(e)
+            return []
+    
+
+    def menu_algorithm() -> callable:
+        print("Selecione o algoritmo:")
+        print("1) Bubble Sort")
+        print("2) Quick Sort")
+        match int(input()):
+            case 1:
+                return BubbleSort()
+            case 2: 
+                return QuickSort()
+            case __default__:
+                return None
+
+
+    def menu_data() -> int:
+        print("Número de dados:")
+        print("1) 1K")
+        print("2) 10K")
+        print("3) 50K")
+        print("4) 80K")
+        print("5) 100K")
+        match int(input()):
+            case 1:
+                return 10**3
+            case 2:
+                return 10**4
+            case 3:
+                return 10**4 * 5
+            case 4:
+                return 10**4 * 8
+            case 5:
+                return 10**5
+
+
+    def menu_iterations() -> int:
+        print("Número de iterações: ")
+        return int(input())
+
+
+    data_list: list = get_data('data.txt')
+    separator: str = "========================="
--- main.py
+++ main.py
@@ -10,62 +10,7 @@ import sys
 sys.setrecursionlimit(1000000) # Aumenta o limite de recursividade (a quantidade de vezes que uma função pode chamar a si mesma)
 # 1K 10K 50K 80K 100K
 
-def get_data(file_name: str) -> list:
-    try:
-        raw: str = ''
-        with open('data.txt') as f:
-            raw = f.read()
-            f.close()
-        raw_list: list = raw.split(',')
-        int_list: list = []
-        for item in raw_list:
-            int_list.append(int(item))
-        return int_list
-    except Exception as e:
-        print(e)
-        return []
-    
 
-def menu_algorithm() -> callable:
-    print("Selecione o algoritmo:")
-    print("1) Bubble Sort")
-    print("2) Quick Sort")
-    match int(input()):
-        case 1:
-            return BubbleSort()
-        case 2: 
-            return QuickSort()
-        case __default__:
-            return None
-    
-
-def menu_data() -> int:
-    print("Número de dados:")
-    print("1) 1K")
-    print("2) 10K")
-    print("3) 50K")
-    print("4) 80K")
-    print("5) 100K")
-    match int(input()):
-        case 1:
-            return 10**3
-        case 2:
-            return 10**4
-        case 3:
-            return 10**4 * 5
-        case 4:
-            return 10**4 * 8
-        case 5:
-            return 10**5
-        
-
-def menu_iterations() -> int:
-    print("Número de iterações: ")
-    return int(input())
-    
-
-data_list: list = get_data('data.txt')
-separator: str = "========================="
 
 while True:
     print(separator)
