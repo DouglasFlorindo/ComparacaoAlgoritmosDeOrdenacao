267c7aade165720a257bd6ef32b0956710e14c57
Douglas Florindo <douglasflorindodeassis@gmail.com>
2024-10-22 18:57:51 -0300

    estado atual da aplicação

--- main.py
+++ main.py
@@ -3,24 +3,68 @@ from SortingAlgorithms.quickSortAntigo import *
 from SortingAlgorithms.quickSort import *
 from Modules.test import *
 import sys
-import random
 
 sys.setrecursionlimit(1000000) # Aumenta o limite de recursividade (a quantidade de vezes que uma função pode chamar a si mesma)
+# 1K 10K 50K 80K 100K
 
-lista = []
+def get_data(file_name: str) -> list:
+    try:
+        raw: str = ''
+        with open('data.txt') as f:
+            raw = f.read()
+            f.close()
+        raw_list: list = raw.split(',')
+        int_list: list = []
+        for item in raw_list:
+            int_list.append(int(item))
+        return int_list
+    except Exception as e:
+        print(e)
+        return []
+    
 
-for i in range(1000):
-    lista.append(i)
+def menu_algorithm() -> callable:
+    print("Selecione o algoritmo:")
+    print("1) Bubble Sort")
+    print("2) Quick Sort")
+    match int(input()):
+        case 1:
+            return BubbleSort()
+        case 2: 
+            return QuickSort()
+        case __default__:
+            return None
+    
 
-random.shuffle(lista)
+def menu_data() -> int:
+    print("Número de dados:")
+    print("1) 1K")
+    print("2) 10K")
+    print("3) 50K")
+    print("4) 80K")
+    print("5) 100K")
+    match int(input()):
+        case 1:
+            return 10**3
+        case 2:
+            return 10**4
+        case 3:
+            return 10**4 * 5
+        case 4:
+            return 10**4 * 8
+        case 5:
+            return 10**5
+        
 
-obj = Test()
-obj.execute_tests(QuickSort(), lista, 1)
-obj.export_results_to_csv()
-# Favor rodar somente um teste por vez ;)
+def menu_iterations() -> int:
+    print("Número de iterações: ")
+    return int(input())
+    
 
-# print(Test().execute_tests(QuickSort(), lista, 500))
-# print(Test().execute_tests(BubbleSort(), lista, 500))
-# print(Test().execute_tests(QuickSortAlt(), lista, 500))
+data_list: list = get_data('data.txt')
+separator: str = "========================="
 
-
+while True:
+    print(separator)
+    
+    break
